7206d3877d0d64ba2cc09f575a9ba0d9c1a0622d
Tanapon0207 <80101335+Tanapon0207@users.noreply.github.com>
2023-04-25 21:58:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://127.0.0.1:27017/admin',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var username = req.body.username;
+    var password = req.body.password;
+    var position = req.body.position;
+    
+
+    var data = {
+        "username": username,
+        "password" : password,
+        "position" : position
+    }
+
+    db.collection('data').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signup_success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(5000);
+
+
+console.log("Listening on PORT 5000");
